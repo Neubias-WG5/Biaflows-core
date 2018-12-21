8db599d86263aafb0ccac36c9ac761ea4a6af8ac
Ulysse Rubens <ulyrubens@hotmail.com>
2018-12-21 11:53:36 +0100

    Add script to locally build NeubiasWG5 core image

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# 1) Update version number BEFORE running this script, in application.properties
+
+# 2) Generate executables
+./grailsw rest-api-doc
+./grailsw war
+
+# 3) Copy to Docker build context
+cp restapidoc.json docker
+cp core.war docker
+
+# 4) Retrieve version number
+VERSION=$(cat application.properties | grep app.version=)
+VERSION=${VERSION#app.version=}
+
+# 5) Build Docker image
+docker build \
+    --build-arg RELEASE_PATH="." \
+    --build-arg FROM_NAMESPACE=cytomineuliege \
+    --build-arg FROM_VERSION=v1.2.0 \
+    -t neubiaswg5/core:latest -t neubiaswg5/core:v$VERSION docker/
+
+# 6) Push image to Docker Hub
+#docker push neubiaswg5/core:latest
+#docker push neubiaswg5/core:v$VERSION
+
+# 7) Update NeubiasWG5/Cytomine-bootstrap with the new Docker image version for core.
